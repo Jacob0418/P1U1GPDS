89ea0c0e11bfb5d5cbe439ef67305155f0b48ff8
Jacob0418 <cjosecoronado97@gmail.com>
2025-10-05 18:32:52 -0500

    apt inject

--- inject-env.sh
+++ inject-env.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
+echo "Running inject-env.sh to replace environment variables in index.html"
+
+echo "VITE_SUPABASE_URL: $VITE_SUPABASE_URL"
+echo "VITE_SUPABASE_ANON_KEY: $VITE_SUPABASE_ANON_KEY"
+
 # Reemplaza las variables de entorno en el archivo index.html
 envsubst '${VITE_SUPABASE_URL} ${VITE_SUPABASE_ANON_KEY}' < /usr/share/nginx/html/index.html > /usr/share/nginx/html/index.html.tmp
 mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
+echo "Environment variables replaced successfully"
 
 exec "$@"
